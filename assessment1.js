429389535b18bb9cc9ff91c6c337ec8074dcabbd
chetanrevankar97 <55571279+chetanrevankar97@users.noreply.github.com>
2019-09-26 12:18:00 +0530

    Add files via upload

---
+++ assessment1.js
@@ -0,0 +1,136 @@
+
+    var total=0;
+    var arr=[];
+    $("#btnm1").click(function(){
+        var text1="1984";
+        var b1=180;
+                
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm2").click(function(){
+        var text1="The Alchemist";
+        var b1=150;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm3").click(function(){
+        var text1="Blade Runner";
+        var b1=200;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm4").click(function(){
+        var text1="Chess Story";
+        var b1=300;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm5").click(function(){
+        var text1="The Alchemist";
+        var b1=200;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm6").click(function(){
+        var text1="The Hobbit There and Back Again";
+        var b1=300;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm7").click(function(){
+        var text1="The Metamorphosis";
+        var b1=350;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm8").click(function(){
+        var text1="The Old Man and the Sea";
+        var b1=200;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm9").click(function(){
+        var text1="Murder on the Orient Express";
+        var b1=250;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm10").click(function(){
+        var text1="The Little Prince";
+        var b1=220;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm11").click(function(){
+        var text1="A Study in Scarlet";
+        var b1=350;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnm12").click(function(){
+        var text1="The Time Machine";
+        var b1=450;
+                    
+                    arr+="<tr><td>"+text1+"</td><td>"+b1+"</td></tr>";
+                    $("#tb1").html(arr);
+                    total+=b1;
+                    $("#ht").html("Total= "+total);
+    });
+
+    $("#btnpo").click(function(){
+        $("#tb2").html(arr);
+         $("#ht2").html("Total= "+total);
+         
+    });
+
+    $("#finbtn").click(function() {
+        alert("Your order has been placed");
+    });
+
+
+
+    
